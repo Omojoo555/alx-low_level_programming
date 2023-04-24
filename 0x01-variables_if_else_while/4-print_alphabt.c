bebf4621a99207869f3267ae0d0d10e852934d83
omojoo555 <benjaminmiracleomojo@gmail.com>
2023-04-24 13:08:35 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+*main - Entry point
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-        char letter;
-
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                if (letter != 'e' && letter != 'q')
-                        putchar(letter);
-        }
-
-        putchar('\n');
-
-        return (0);
+int n = 97;
+while (n <= 122)
+{
+if (n != 'q' && n != 'e')
+{
+putchar(n);
+}
+n++;
+}
+putchar('\n');
+return (0);
 }
